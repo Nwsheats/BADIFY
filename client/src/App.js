b75675a2cee3fbec0a51a3d689d38b409822431e
Melisa Z <melzahirovic@gmail.com>
2023-02-13 21:50:28 -0600

    forum commented out

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
+//import Forum from './pages/Forum';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
