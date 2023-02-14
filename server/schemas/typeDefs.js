0697062f7872c5088e7aa3c5a588e2de70b0b2b2
Conor Donnelly <donn0021@umn.edu>
2023-02-13 22:05:35 -0600

    started drafting type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,55 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        firstname: String
+        lastname: String
+        email: String
+        createdAt: String
+        comments: [Comment]
+    }
 
+    type Song {
+        _id: ID
+        songDay: Number
+        songTitle: String
+        artistName: String
+        recordLabel: String
+        releaseYear: Number
+        createdAt: String
+        comments: [Comment]
+        commentCount: Number
+    }
+
+    type Comment {
+        userName: String
+        commentText: String
+        createdAt: String
+        songId: [Song]
+    }
+
+    type Playlist {
+        username: String
+        listName: String
+        createdAt: String
+        songs: [Song]
+        
+    }
 
     type Auth {
         token: ID!
         user: user
     }
 
+    type Query {
+    }
+
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
+       
     }
 `;
 
