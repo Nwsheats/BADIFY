e628928e24ab69880bd27d9c2a6d661c9c83a877
moony <moonyxiong@gmail.com>
2023-02-18 09:23:30 -0600

    Fix: Merge Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import logo from "./logo.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
 
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
+} from '@apollo/client';
 
-import { setContent, setContext } from "@apollo/client/link/context";
+import { setContent, setContext } from '@apollo/client/link/context';
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 import Home from './pages/Home'; 
 import Login from './pages/Login';
@@ -23,17 +24,19 @@ import Profile from './pages/Profile';
 import DailySong from './components/DailySong';
 //import Forum from './pages/Forum';
 
+
+
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
+  const token = localStorage.getItem('id_token');
 
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
